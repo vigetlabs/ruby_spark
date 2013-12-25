f90c47057cbf901dd462c14d94c20c8c670009be
Eli Fatsi <eli.fatsi@viget.com>
2013-12-24 23:43:03 -0700

    Handling errors and added integration specs

--- lib/ruby_spark/core.rb
+++ lib/ruby_spark/core.rb
@@ -1,5 +1,6 @@
 module RubySpark
   class Core
+    class ApiError < StandardError; end
 
     def initialize(core_id)
       raise RubySpark::AuthTokenNotDefinedError if RubySpark.auth_token.nil?
@@ -9,22 +10,30 @@ module RubySpark
 
     def digital_write(pin, message)
       response = post('digitalwrite', "D#{pin},#{message}")
-      response.parsed_response["return_value"] == 1
+      handle(response) do
+        response["return_value"] == 1
+      end
     end
 
     def digital_read(pin)
       response = post('digitalread', "D#{pin}")
-      response.parsed_response["return_value"] == 1 ? "HIGH" : "LOW"
+      handle(response) do
+        response["return_value"] == 1 ? "HIGH" : "LOW"
+      end
     end
 
     def analog_write(pin, value)
       response = post('analogwrite', "A#{pin},#{value}")
-      response.parsed_response["return_value"] == 1
+      handle(response) do
+        response["return_value"] == 1
+      end
     end
 
     def analog_read(pin)
       response = post('analogread', "A#{pin}")
-      response.parsed_response["return_value"]
+      handle(response) do
+        response["return_value"]
+      end
     end
 
     private
@@ -33,7 +42,23 @@ module RubySpark
       url  = base_url + action
       body = access_params.merge(:params => params)
 
-      response = HTTParty.post(url, :body => body)
+      HTTParty.post(url, :body => body).parsed_response
+    end
+
+    def handle(response, &block)
+      if error = error_from(response)
+        raise ApiError.new(error) if error.length > 0
+      else
+        yield block
+      end
+    end
+
+    def error_from(response)
+      response["error"].tap do |error|
+        description = response["error_description"]
+        error.concat(": #{description}") if description
+        error.concat(": You do not have access to that Core") if error == "Permission Denied"
+      end
     end
 
     def base_url
---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'vcr'
+
+require 'ruby_spark' # and any other gems you need
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.default_cassette_options = {
+    :record                 => :once,
+    :allow_playback_repeats => true,
+  }
+  c.allow_http_connections_when_no_cassette = false
+end
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
