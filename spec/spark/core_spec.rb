a41c2044559f8b40e342afe26a6a244bf7d8b309
Eli Fatsi <eli.fatsi@viget.com>
2014-02-20 14:56:25 -0700

    Added Core specs

---
+++ spec/spark/core_spec.rb
@@ -0,0 +1,105 @@
+require 'spec_helper'
+require 'pry'
+
+describe RubySpark::Core do
+
+  context "when things go right" do
+    before  { RubySpark.access_token = "good_access_token" }
+    subject { described_class.new("good_core_id") }
+
+    describe "#info" do
+      it "succeeds when Access Token and Core ID are correct" do
+        VCR.use_cassette("info") do
+          info = subject.info
+
+          info.should be_a Hash
+          info.keys.should =~ ["id", "name", "variables", "functions"]
+        end
+      end
+    end
+
+    describe "#variable" do
+      it "succeeds when Access Token and Core ID are correct" do
+        VCR.use_cassette("variable") do
+          subject.variable("temperature").should == 70
+        end
+      end
+    end
+
+    describe "#function" do
+      it "succeeds when Access Token and Core ID are correct" do
+        VCR.use_cassette("function") do
+          subject.function("readTemp", "outside").should == 72
+        end
+      end
+    end
+  end
+
+  context "when things go wrong" do
+    it "returns the appropriate error when Access Token is bad" do
+      RubySpark.access_token = "bad_token"
+      subject = described_class.new("good_core_id")
+
+      VCR.use_cassette("bad_token") do
+        expect {
+          subject.info
+        }.to raise_error(RubySpark::Core::ApiError)
+      end
+
+      VCR.use_cassette("bad_token") do
+        begin
+          subject.info
+        rescue => e
+          e.message.should == "invalid_grant: The access token provided is invalid."
+        end
+      end
+    end
+
+    it "returns proper error if Access Token is not defined" do
+      RubySpark.access_token = nil
+
+      expect {
+        subject = described_class.new("good_core_id")
+      }.to raise_error(RubySpark::ConfigurationError)
+    end
+
+    it "returns the appropriate error when Core ID is bad" do
+      RubySpark.access_token = "good_access_token"
+      subject = described_class.new("bad_core_id")
+
+      VCR.use_cassette("bad_core_id") do
+        expect {
+          subject.info
+        }.to raise_error(RubySpark::Core::ApiError)
+      end
+
+      VCR.use_cassette("bad_core_id") do
+        begin
+          subject.info
+        rescue => e
+          e.message.should == "Permission Denied: Invalid Core ID"
+        end
+      end
+    end
+
+    it "returns the appropriate error when the Spark API times out" do
+      RubySpark.access_token = "good_access_token"
+      subject = described_class.new("good_core_id")
+
+      VCR.use_cassette("spark_timeout") do
+        expect {
+          subject.info
+        }.to raise_error(RubySpark::Core::ApiError)
+      end
+
+      VCR.use_cassette("spark_timeout") do
+        begin
+          subject.info
+        rescue => e
+          e.message.should == "Timed out."
+        end
+      end
+    end
+  end
+
+end
--- spec/spark/tinker_spec.rb
+++ spec/spark/tinker_spec.rb
@@ -13,58 +13,6 @@ describe RubySpark::Tinker do
           subject.digital_write(7, "HIGH").should == true
         end
       end
-
-      it "returns the appropriate error when Access Token is bad" do
-        RubySpark.access_token = "bad_token"
-
-        VCR.use_cassette("bad_token") do
-          expect {
-            subject.digital_write(7, "HIGH")
-          }.to raise_error(RubySpark::Core::ApiError)
-        end
-
-        VCR.use_cassette("bad_token") do
-          begin
-            subject.digital_write(7, "HIGH")
-          rescue => e
-            e.message.should == "invalid_grant: The access token provided is invalid."
-          end
-        end
-      end
-
-      it "returns the appropriate error when Core ID is bad" do
-        subject = described_class.new("bad_core_id")
-
-        VCR.use_cassette("bad_core_id") do
-          expect {
-            subject.digital_write(7, "HIGH")
-          }.to raise_error(RubySpark::Core::ApiError)
-        end
-
-        VCR.use_cassette("bad_core_id") do
-          begin
-            subject.digital_write(7, "HIGH")
-          rescue => e
-            e.message.should == "Permission Denied: Invalid Core ID"
-          end
-        end
-      end
-
-      it "returns the appropriate error when the Spark API times out" do
-        VCR.use_cassette("spark_timeout") do
-          expect {
-            subject.digital_write(7, "HIGH")
-          }.to raise_error(RubySpark::Core::ApiError)
-        end
-
-        VCR.use_cassette("spark_timeout") do
-          begin
-            subject.digital_write(7, "HIGH")
-          rescue => e
-            e.message.should == "Timed out."
-          end
-        end
-      end
     end
 
     describe "#digital_read" do
@@ -87,39 +35,6 @@ describe RubySpark::Tinker do
       it "succeeds when Access Token and Core ID are correct" do
         VCR.use_cassette("analog_read") do
           subject.analog_read(6).should == 2399
-        end
-      end
-    end
-  end
-
-  context "with Access Token passed into Core" do
-    subject { described_class.new("good_core_id", "good_access_token") }
-
-    describe "#digital_read" do
-      it "succeeds when Access Token and Core ID are correct" do
-        VCR.use_cassette("digital_write") do
-          subject.digital_write(7, "HIGH").should == true
-        end
-      end
-    end
-  end
-
-  context "with no Access Token defined" do
-    before  { RubySpark.access_token = nil }
-    subject { described_class.new("good_core_id") }
-
-    it "returns proper error if Access Token is not defined" do
-      VCR.use_cassette("no_token") do
-        expect {
-          subject.digital_read(6)
-        }.to raise_error(RubySpark::ConfigurationError)
-      end
-
-      VCR.use_cassette("no_token") do
-        begin
-          subject.digital_read(6)
-        rescue => e
-          e.message.should == "Access Token not defined"
         end
       end
     end
