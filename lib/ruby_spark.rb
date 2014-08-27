38dbbc0a03ca4826eafa0e4be3000752b8d0b70b
Eli Fatsi <eli.fatsi@viget.com>
2014-08-27 16:31:07 -0600

    Using extend, moving Configuration to top level helpers directory

--- lib/ruby_spark.rb
+++ lib/ruby_spark.rb
@@ -3,12 +3,12 @@ require 'httparty'
 require 'ruby_spark/version'
 require 'ruby_spark/core'
 require 'ruby_spark/tinker'
-require 'ruby_spark/helpers/configuration'
+require 'helpers/configuration'
 
 module RubySpark
   class ConfigurationError < StandardError; end
 
-  include Configuration
+  extend Configuration
 
   define_setting :access_token
   define_setting :timeout, 30
